b44ad9d3bd77b829ec5c238b1a8121517be19766
KatyaLehedz <99407798+KatyaLehedz@users.noreply.github.com>
2023-05-16 13:34:39 +0300

    Add files via upload

---
+++ Lab3/Lab3/Program.cs
@@ -0,0 +1,75 @@
+﻿using Lab3;
+using System.Numerics;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+        Console.WriteLine("Введіть a, b та c для рівняння ax^2 + bx + c = 0.");
+        Console.Write("a:");
+        double a = int.Parse(Console.ReadLine());
+        Console.Write("b:");
+        double b = int.Parse(Console.ReadLine());
+        Console.Write("c:");
+        double c = int.Parse(Console.ReadLine());
+        Solver solver = new Solver(a, b, c);
+
+        while (true)
+        {
+            Console.WriteLine("\nКвадратне рівняння: {0}x^2 {3}{1}x {4}{2} = 0",
+                a, b, c, (b >= 0 ? "+ " : ""), (c >= 0 ? "+ " : ""));
+
+            Console.WriteLine();
+            Console.WriteLine("1. Знайти розв'язок в просторі R.");
+            Console.WriteLine("2. Знайти розв'язок в просторі комплексних чисел.");
+            Console.WriteLine("3. Змінити дані для квадратного рівняння.");
+            Console.WriteLine("0. Вихід.");
+
+            Console.Write("Виберіть простір: ");
+            int choice = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            switch (choice)
+            {
+                case 1:
+                    Console.Write("Розв'язок в просторі R.\n\n");
+
+                    double[] realResult = ((EquationSolver)solver).SolveReal();
+
+                    Console.WriteLine("Розв'язки квадратного рівняння: x1 = {0}, x2 = {1}", realResult[0], realResult[1]);
+                    break;
+
+                case 2:
+                    Console.Write("Розв'язок в просторі R.\n\n");
+
+                    Complex[] complexResult = ((ComplexEquationSolver)solver).SolveComplex();
+
+                    Console.WriteLine("Розв'язки квадратного рівняння: x1 = {0}, x2 = {1}",
+                        Solver.ToStringComplex(complexResult[0]),
+                        Solver.ToStringComplex(complexResult[1]));
+                    break;
+
+                case 3:
+                    Console.WriteLine("\nВведіть a, b та c для рівняння ax^2 + bx + c = 0.");
+                    Console.Write("a:");
+                    a = int.Parse(Console.ReadLine());
+                    Console.Write("b:");
+                    b = int.Parse(Console.ReadLine());
+                    Console.Write("c:");
+                    c = int.Parse(Console.ReadLine());
+                    solver.ChangeData(a, b, c);
+                    break;
+
+                case 0:
+                    Environment.Exit(0);
+                    break;
+
+                default:
+                    Console.WriteLine("Нема такого варіанту");
+                    break;
+            }
+        }
+    }
+}
---
+++ Lab3/Lab3/Solver.cs
@@ -0,0 +1,67 @@
+﻿using System.Numerics;
+
+namespace Lab3
+{
+    internal class Solver : EquationSolver, ComplexEquationSolver
+    {
+        private double a;
+        private double b;
+        private double c;
+
+        public Solver(double a, double b, double c)
+        {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+        }
+
+        public void ChangeData(double a, double b, double c)
+        {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+        }
+
+        public double Discriminant()
+        {
+            return b * b - 4 * a * c;
+        }
+
+        public double[] SolveReal()
+        {
+            double discriminant = Discriminant();
+            if (discriminant < 0)
+            {
+                Console.WriteLine("Дискримінант менше 0, рівняння не має розв'язків");
+                return new double[] { 0, 0 };
+            }
+            double x1 = (-b + Math.Sqrt(discriminant)) / (2 * a);
+            double x2 = (-b - Math.Sqrt(discriminant)) / (2 * a);
+            return new double[] { x1, x2 };
+        }
+
+        public Complex[] SolveComplex()
+        {
+            double discriminant = Discriminant();
+            Complex x1 = new Complex(-b / (2 * a), Math.Sqrt(-discriminant) / (2 * a));
+            Complex x2 = new Complex(-b / (2 * a), -Math.Sqrt(-discriminant) / (2 * a));
+            return new Complex[] { x1, x2 };
+        }
+
+        public static string ToStringComplex(Complex complex)
+        {
+            return complex.Real.ToString("0.00") + (complex.Imaginary >= 0 ? " + " : " ") + complex.Imaginary.ToString("0.00") + "i";
+        }
+    }
+
+    public interface EquationSolver
+    {
+        double[] SolveReal();
+    }
+
+    public interface ComplexEquationSolver
+    {
+        Complex[] SolveComplex();
+    }
+
+}
